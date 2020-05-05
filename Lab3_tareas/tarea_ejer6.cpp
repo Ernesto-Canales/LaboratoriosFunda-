c7162630565150eb96372acdd5e07a06c87f8e0c
Ernesto-Canales <00051120@uca.edu.sv>
2020-05-05 13:55:46 -0600

    Tarea_add_labo3

---
+++ Lab3_tareas/tarea_ejer6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    double price;
+    int amount;
+
+    cout << "Nombre del producto: \n";
+    cin >> name;
+    cout << "Precio del producto: \n";
+    cin >> price;
+    cout << "Cantidad del producto: \n";
+    cin >> amount;
+
+    if (price < 0) {
+        cout << "El precio del producto no puede menor que 0";
+    } else if (amount < 0) {
+        cout << "La cantidad del producto no puede menor que 0";
+    } else {
+        double total = (price*amount);
+
+        cout << "En la compra de " << amount << " " << name << " a $" << price << ", se gasto un total de $" << total << " Dolares.";
+    }
+
+    return 0;
+
+}
