5c1eabd5996eb1db8736d86ec34f0bd34b965eca
Ernesto-Canales <00051120@uca.edu.sv>
2020-06-21 10:04:30 -0600

    Optimizar ejercicio 6

--- Laboratorio6_tareas/tarea6_ejer2_00051120.cpp
+++ Laboratorio6_tareas/tarea6_ejer2_00051120.cpp
@@ -4,6 +4,9 @@ using namespace std;
 //Prototipo de generar arreglo
 int gen_array(int [], int);
 
+//Prototipo de mostrar arreglo
+int show_array(int [], int);
+
 //Programa principal - 100 numeros impares
 int main() {
     int n = 1; //Contador de numeros impares
@@ -12,11 +15,8 @@ int main() {
     cout << "El programa muestra los primeros 100 numeros impares de forma descendente\n\n";
 
     gen_array(num_cien, n);
-
-    for (int f = 99; f >= 0; --f) {
-        cout << "N " << c << ". " << num_cien[f] << endl;
-        c += 1;
-    }
+    
+    show_array(num_cien, c);
 }
 
 //Funcion de generar arreglo
@@ -24,5 +24,13 @@ int gen_array(int num_cien[], int n) {
     for (int i = 0; i < 100; ++i) {
         num_cien[i] = n;
         n += 2;
+    }
+}
+
+//Funcion de mostrar arreglo
+int show_array(int num_cien[], int c) {
+    for (int f = 99; f >= 0; --f) {
+        cout << "N " << c << ". " << num_cien[f] << endl;
+        c += 1;
     }
 }
